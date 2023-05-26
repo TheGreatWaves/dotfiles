190bb2811c238b24191ef455ee30e2a898e6421b
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-05-26 23:54:25 +0700

    fixed install script

--- install.sh
+++ install.sh
@@ -10,11 +10,12 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 sudo apt-get install build-essential
 brew doctor
 brew update
+brew install zsh
 
 # So we use all of the packages we are about to install
-echo "export PATH='/usr/local/bin:$PATH'\n" >> ~/.bashrc
-source ~/.bashrc
-
+touch ~/.bashrc
+echo "export PATH='/usr/local/bin:$PATH'\n" >> ~/.bashrc && source ~/.bashrc
+ls -la ~/
 # Install Oh-My-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
@@ -22,7 +23,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 #==============
 # Remove old dot flies
 #==============
-sudo rm -rf ~/.bashrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
@@ -34,6 +34,7 @@ sudo rm -rf ~/Brewfile > /dev/null 2>&1
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/Brewfile ~/Brewfile
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+mkdir -p ~/.config/helix 
 ln -s ~/dotfiles/helix/config.toml ~/.config/helix/config.toml
 
 cd ~
@@ -44,5 +45,5 @@ cd -
 # Set zsh as the default shell
 #==============
 chsh -s /bin/zsh
-
+source ~/.bashrc
 echo -e "\n====== Installation Complete ======\n"
