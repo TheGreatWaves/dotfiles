8b4c268e300fd35ad119c51583fdc7f8f6692629
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-05-29 13:11:17 +0700

    Added keybinding for jumping between input and output panes

--- workspaces/genws.sh
+++ workspaces/genws.sh
@@ -9,4 +9,6 @@ tmux select-pane -t 1
 tmux resize-pane -t 1 -R 40
 tmux resize-pane -t 2 -D 6
 tmux send-keys 'hx' C-m 'Space' F
+tmux bind -n C-o select-pane -t 3
+tmux bind -n C-i select-pane -t 1
 tmux a
