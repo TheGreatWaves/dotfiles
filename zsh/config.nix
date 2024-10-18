532490d4ba69c3c484c82caacf05242eb7f50843
kanagawa <ochawinappi@gmail.com>
2024-10-18 20:52:14 +0700

    setup autosuggestion from zsh default config, not plugin

--- zsh/config.nix
+++ zsh/config.nix
@@ -6,17 +6,7 @@
     enable = true;
     theme = "sorin";
   };
-  plugins = [
-      {
-        name = "zsh-autosuggestions";
-        src = pkgs.fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-autosuggestions";
-          rev = "v0.4.0";
-          sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-        };
-      }
-  ];
+  autosuggestion.enable = true;
   # This is a fix for nixos-wsl, for some reason when using zsh, 
   # sudo is broken because /run/wrappers isn't mounted yet.
   # https://www.reddit.com/r/NixOS/comments/1etw4ip/help_please_wrong_sudo_path_when_zsh_enabled/
